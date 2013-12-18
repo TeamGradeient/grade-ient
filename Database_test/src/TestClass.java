fe26442003504c18bae88059a9acbdf7c12e2eec
zroux93 <zachary.roux@ou.edu>
2013-12-18 11:05:59 -0600

    A placeholder class

---
+++ Database_test/src/TestClass.java
@@ -0,0 +1,7 @@
+
+//There is really no point at all to this class. I just put it here so that there would be something in the Source 
+//folder to commit. Once there is another class, this one should be deleted. :)
+
+public class TestClass {
+
+}
