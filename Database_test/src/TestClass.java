1e8116e0b56d1e1624dad7e1d581d0de7b852497
elizabeth.craig <elizabeth.craig@ou.edu>
2013-12-19 11:39:59 -0600

    Deleted TestClass and fixed joda-time build setup

--- Database_test/src/TestClass.java
+++
@@ -1,9 +0,0 @@
-
-//There is really no point at all to this class. I just put it here so that there would be something in the Source 
-//folder to commit. Once there is another class, this one should be deleted. :)
-
-public class TestClass {
-	public String test;
-	//NO
-	//public int secondTest; 
-}
