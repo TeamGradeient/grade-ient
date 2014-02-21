c9e7d113c3fe735c34d02680bb3d662544cfdfb4
Elizabeth Craig <ecraig12345@gmail.com>
2014-02-21 15:19:08 -0600

    Added very unfinished grade(ient)-test project

---
+++ grade(ient)-test/src/edu/ou/gradeient/test/TaskTest.java
@@ -0,0 +1,30 @@
+package edu.ou.gradeient.test;
+
+import junit.framework.TestCase;
+
+public class TaskTest extends TestCase {
+	public TaskTest() {
+		
+	}
+	
+	@Override
+	protected void setUp() {
+		
+	}
+	
+	@Override
+	protected void tearDown() {
+		
+	}
+
+	// TODO This is just an example--delete later
+	public void testTrue() {
+		assertTrue(true);
+	}
+	
+	// TODO This is just an example--delete later
+	public void testAdd() {
+		assertEquals(4, 2 + 2);
+		assertEquals(3, 2 + 2);
+	}
+}
