03adadd3c9c53c625bf4fe06c5b19988e8899f67
zroux93 <zachary.roux@ou.edu>
2014-02-20 19:23:52 -0600

    Update of TaskModel

--- grade(ient)/src/edu/ou/gradeient/TaskModel.java
+++ grade(ient)/src/edu/ou/gradeient/TaskModel.java
@@ -1,13 +1,11 @@
 package edu.ou.gradeient;
 
-import java.util.TreeMap;
-
-import org.joda.time.DateTime;
+import java.util.ArrayList;
 
 public class TaskModel 
 {
-	/**TreeMap with key of due date and value of Task**/
-	TreeMap<DateTime, Task> taskList = new TreeMap<DateTime, Task>();
+	/**ArrayList to store Task objects**/
+	ArrayList<Task> taskList = new ArrayList<Task>();
 	
 	/**
 	 * Adds a new task to the model.
@@ -15,8 +13,25 @@ public class TaskModel
 	 */
 	public void addTask (Task newTask)
 	{
-		taskList.put(newTask.getTaskInterval().getEnd(), newTask);
+		taskList.add(newTask);
 		System.out.println("A new task has been added.");
+	}
+	
+	/**
+	 * Removes a task from the model.
+	 * @param toDelete The task to be deleted
+	 * @return True if the task was deleted, false 
+	 * if the task did not exist in the model. 
+	 */
+	public boolean removeTask (Task toDelete)
+	{
+		return taskList.remove(toDelete);
+	}
+	
+	public Task[] getTaskList ()
+	{
+		Task [] list = new Task [10];
+		return taskList.toArray(list);
 	}
 
 }
