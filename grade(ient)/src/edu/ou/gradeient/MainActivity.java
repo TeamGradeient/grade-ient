43f2e5553c7daa20f89cf333316f7b905b48759c
zroux93 <zachary.roux@ou.edu>
2014-03-06 17:34:33 -0600

    Added writeModelToFile() and readModelFromFile() methods, and some code that demonstrates writing the model to a file and reading it back in.

--- grade(ient)/src/edu/ou/gradeient/MainActivity.java
+++ grade(ient)/src/edu/ou/gradeient/MainActivity.java
@@ -3,17 +3,79 @@ package edu.ou.gradeient;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+import java.io.*;
 
+public class MainActivity extends Activity {
+	
+	private static final String TAG = "edu.ou.gradeient.MainActivity";
+	private TaskModel model;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		new Database(this).getWritableDatabase();
+		
+		//Construct an instance of the model
+		model = new TaskModel();
+		
+		/*----------------testing for model----------------*/
+		
+		//Add some example tasks to the model for testing
+		model.addTask(new Task("Test task 1", 0, 1));
+		model.addTask(new Task("Test task 2", 3, 4));
+		model.addTask(new Task("Test task 3", 3, 4));
+		
+		//Serialize the model and write it to a file on the Android file system
+		writeModelToFile("testFile");
+		
+		//Set model to null
+		model = null;
+		
+		//Reads file from Android file system
+		readModelFromFile("testFile");
+		
+		//Prints names of tasks
+		Task[] myTasks = model.getTaskList();
+		for (int i = 0; i < myTasks.length; ++i)
+		{
+			System.out.println(myTasks[i].getName());
+		}
+		
+		/*----------------end of testing----------------*/
+		
 		setContentView(R.layout.activity_main);
-		//Hello!
+	}
+
+	private void writeModelToFile(String fileName) {
+		try {
+		     FileOutputStream fileOut = openFileOutput(fileName, MODE_PRIVATE);
+		     ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		     out.writeObject(model);
+		     out.close();
+		     fileOut.close();
+		     Log.d(TAG, "Serialized data is saved in testFile");
+	    } catch(IOException i){
+	          i.printStackTrace();
+	    }
+	}
+
+	private void readModelFromFile(String fileName) {
+		try {
+		     FileInputStream fileIn = openFileInput(fileName);
+		     ObjectInputStream in = new ObjectInputStream(fileIn);
+		     model = (TaskModel) in.readObject();
+		     in.close();
+		     fileIn.close();
+		     Log.d(TAG, "Serialized data has been loaded from testFile");
+	    } catch(IOException i){
+	    	i.printStackTrace();
+	    } catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -26,10 +88,14 @@ public class MainActivity extends Activity {
 	public void showTaskView (View view)
 	{
 		//Do something in response to button being pressed
-		System.out.println("The button was pressed.");
+		Log.d(TAG, "The button was pressed.");
 		
 		Intent intent = new Intent (this, TaskView.class);
+		intent.setData(null);
 		startActivity(intent);
+		
+		View myView = getLayoutInflater().inflate(R.layout.activity_task_view, 
+				null);
 	}
 
 }
