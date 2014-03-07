43dd619f2b33086cd69313bf41d0152184374990
zroux93 <zachary.roux@ou.edu>
2014-03-07 15:17:08 -0600

    Displays two rows now!

--- grade(ient)/src/edu/ou/gradeient/TaskView.java
+++ grade(ient)/src/edu/ou/gradeient/TaskView.java
@@ -2,22 +2,58 @@ package edu.ou.gradeient;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.app.ListFragment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.*;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.os.Build;
 
-public class TaskView extends Activity {
-
+public class TaskView extends ListActivity {
+	
+	private static final String TAG = "edu.ou.gradeient.TaskView";
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		Task[] tasks = MainActivity.getModel().getTaskList();
+		for (int i = 0; i < tasks.length; ++i)
+		{
+			System.out.println(tasks[i].getName());
+		}
+		
 		setContentView(R.layout.activity_task_view);
 		// Show the Up button in the action bar.
 		setupActionBar();
-	}
+		
+		//Set up an ArrayAdapter with data from model.
+		//This adapter contains the names of the tasks.
+		ArrayAdapter<Task> arrayAdapter = new ArrayAdapter<Task>(this,
+				android.R.layout.simple_list_item_2, android.R.id.text1, 
+				MainActivity.getModel().getTaskList()) {
+					  @Override
+					  public View getView(int position, View convertView, ViewGroup parent) {
+					    View view = super.getView(position, convertView, parent);
+					    TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+					    TextView text2 = (TextView) view.findViewById(android.R.id.text2);
 
+					    text1.setText(MainActivity.getModel().getTaskList()[position].getName());
+					    text2.setText(MainActivity.getModel().getTaskDueDates()[position]);
+					    return view;
+					  }
+					};
+		
+		setListAdapter(arrayAdapter);
+		//setListAdapter(taskDateAdapter);
+	}
+	
 	/**
 	 * Set up the {@link android.app.ActionBar}, if the API is available.
 	 */
@@ -28,7 +64,6 @@ public class TaskView extends Activity {
 		}
 	}
 
-	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.task_view, menu);
